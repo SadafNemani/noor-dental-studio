0aff3ca7668114617bef724b1eea8920395f2e85
SadafNemani <sedefnemani@gmail.com>
2026-09-09 00:32:18 +0330

    created a custom hook for computing direction based on site's language

---
+++ src/hooks/useDirection.ts
@@ -0,0 +1,9 @@
+"use client";
+
+import { useLocale } from "next-intl";
+
+export function useDirection() {
+  const locale = useLocale();
+  const isRTL = locale === "ar";
+  return { locale, isRTL, dir: isRTL ? "rtl" : "ltr" } as const;
+}
