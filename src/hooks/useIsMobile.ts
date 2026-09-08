f860b512195b04d1ac26a661454de7f54ffb60a9
SadafNemani <sedefnemani@gmail.com>
2026-09-09 01:11:57 +0330

    created reduced motion hook and mobile recognition hook

---
+++ src/hooks/useIsMobile.ts
@@ -0,0 +1,24 @@
+"use client";
+
+import { useSyncExternalStore } from "react";
+
+const BREAKPOINT = 768;
+const QUERY = `(max-width: ${BREAKPOINT - 1}px)`;
+
+function subscribe(callback: () => void) {
+  const mq = window.matchMedia(QUERY);
+  mq.addEventListener("change", callback);
+  return () => mq.removeEventListener("change", callback);
+}
+
+function getSnapshot() {
+  return window.matchMedia(QUERY).matches;
+}
+
+function getServerSnapshot() {
+  return false;
+}
+
+export function useIsMobile() {
+  return useSyncExternalStore(subscribe, getSnapshot, getServerSnapshot);
+}
