6ce4f221148cd1aa0a1303bbb6ac66d19abaf0ab
SadafNemani <sedefnemani@gmail.com>
2026-09-15 22:56:15 +0330

    feat: added em tag support

--- src/lib/richText.tsx
+++ src/lib/richText.tsx
@@ -1,5 +1,7 @@
 export const richText = {
   accent: (chunks: React.ReactNode) => <span className="text-gold">{chunks}</span>,
 
+  em: (chunks: React.ReactNode) => <span className="approach-word">{chunks}</span>,
+
   br: () => <br />,
 };
